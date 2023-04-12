e6b84575db4c07b55c105a504b1724b9620d4b9b
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-04-13 02:15:06 +0900

    feat: setState 구현

--- src/classes/components/Child2.tsx
+++ src/classes/components/Child2.tsx
@@ -8,7 +8,11 @@ class Child2 extends React.Component {
     const globalState = this.context as GlobalState;
     return (
       <div>
-        ------- 자식(뎁스2) {globalState.value2}
+        ------- 자식(뎁스2)
+        <div>
+          value1 : {JSON.stringify(globalState.value1)}
+          <button onClick={() => globalState.setValue1(!globalState.value1)}>불리안 업데이트</button>
+        </div>
       </div>
     );
   }
--- src/classes/provider/GlobalProvider.tsx
+++ src/classes/provider/GlobalProvider.tsx
@@ -3,12 +3,14 @@ import React from "react";
 export interface GlobalState {
   value1: boolean;
   value2: string;
+  setValue1: (value: boolean) => void;
 }
 
 
 export const GlobalContext = React.createContext<GlobalState>({
   value1: false,
   value2: '',
+  setValue1: () => { },
 });
 
 
@@ -20,6 +22,7 @@ interface GlobalProviderProps {
 class GlobalProvider extends React.Component<GlobalProviderProps, GlobalState> {
   state: GlobalState = {
     value1: false,
+    setValue1: (newValue) => this.setState({ value1: newValue }),
     value2: '하이',
   };
 
